c7d95adfe3fdc225f99557823e8cd2045750cdbb
Brando Meniconi <b.meniconi@silverbackstudio.it>
2020-07-10 12:47:22 +0000

    add logging facility

---
+++ tests/logging.test.js
@@ -0,0 +1,25 @@
+const { log } = require("../lib/logging");
+
+describe('Test logger', () => {
+  let env;
+
+  beforeAll(async () => {
+    env = process.env.NODE_ENV;
+
+    // this prevents the console output killswitch in testing
+    process.env['NODE_ENV'] = 'test2';
+  });
+
+  afterAll(async () => {
+    process.env['NODE_ENV'] = env;
+  });
+
+  it('log the message', async () => {
+    const consoleMock = jest.spyOn(console, 'log').mockImplementation();
+
+    log('something');
+    expect(console.log).toHaveBeenLastCalledWith('something');
+
+    consoleMock.mockRestore();
+  });
+});
